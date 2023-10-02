0930bdcf47e29bfa0f45ea5ce25e935e79f461a3
Markus <ztawiz@gmail.com>
2023-10-02 14:08:33 +0200

    Lyckats med appen till arduino, ska testa radio

--- WeatherModule/WeatherModule.ino
+++ WeatherModule/WeatherModule.ino
@@ -1,5 +1,9 @@
 #include <DHT.h>
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
 
+#define diodPin 9                     // Blink diod for testing
 #define DHTPIN 4                      // Temp/humid pin
 #define DHTTYPE DHT22                 // Type of sensor
 DHT dht(DHTPIN, DHTTYPE);             // Create dht object
@@ -9,11 +13,21 @@ void vattnaBlomma(void);              // Waters the flower
 void getBlomfukt(void);               // Gets the moisture from the flower pot
 void lampOn(void);                    // Turn lamp on
 void lampOff(void);                   // Turn lamp off
+void testFunction(void);
+
+RF24 radio(7, 8);
+const byte address[6] = "00001";
 
 void setup() {
   
+  pinMode(9, OUTPUT);
   Serial.begin(9600);
   dht.begin();
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.stopListening();
+  SPI.begin();
 
 }
 
@@ -48,17 +62,31 @@ void printTemp(){
 }
 
 void vattnaBlomma(){
-
+  testFunction();
+  
 }
 
 void getBlomfukt(void){
-
+  testFunction();
+  testFunction();
 }
 
 void lampOn(void){
-
+  uint8_t buf = 1;
+  radio.write(&buf, 1);
 }
 
 void lampOff(void){
-
+  testFunction();
+  testFunction();
+  testFunction();
+  testFunction();
 }
+
+void
+ testFunction(void){
+  digitalWrite(diodPin, 1);
+  delay(500);
+  digitalWrite(diodPin, 0);
+  delay(500);
+ }
